56ab0947562cf9a8e863f5724b5d5c57522d82c6
Devin Campana <devin.campana@me.com>
2022-04-07 09:41:08 -0400

    added us to can

--- moneyConversion/ViewController.swift
+++ moneyConversion/ViewController.swift
@@ -9,6 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayNumber: UILabel!
+    
+    @IBOutlet weak var enterAmount: UITextField!
+    
+    
+    @IBAction func usToCanButton(_ sender: Any) {
+        let numberString = enterAmount.text!
+        let number = Double(numberString)
+        
+        let usToCanResult = (number!) * 1.26
+        let canRoundResult = (usToCanResult * 10) / 10.00
+        displayNumber.text = "🇺🇸$\(numberString) is C$ \(canRoundResult) in Canada🇨🇦"
+    }
+    
+    
+    @IBAction func canToUSButton(_ sender: Any) {
+        let numberString = enterAmount.text!
+        let number = Double(numberString)
+        
+        let canToUsResult = (number!) * 0.8
+        let usRoundResult = (canToUsResult * 10) / 10.00
+        displayNumber.text = "🇨🇦C$\(numberString) is $\(canToUsResult) in the US🇺🇸"
+    }
+    
+    
+    
+    
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
